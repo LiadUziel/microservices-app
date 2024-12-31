4b8d917055bc96370122ba93b3869a90965adbd7
Liad Uziel <liad.uz@gmail.com>
2024-12-31 16:19:11 +0200

    CommentCreateComponent

--- client/src/posts/PostList.tsx
+++ client/src/posts/PostList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import CommentList from "../comments/CommentList";
+import CommentCreate from "../comments/CommentCreate";
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
@@ -25,6 +26,7 @@ const PostList = () => {
         <div className="card-body">
           <h3>{post.title}</h3>
           <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
