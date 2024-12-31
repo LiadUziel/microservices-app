13dded41179621790c47b0931009dbe91d1e85ef
Liad Uziel <liad.uz@gmail.com>
2024-12-31 16:15:44 +0200

    PostCreate Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import PostCreate from "./posts/PostCreate";
 
 function App() {
   return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
     <>
       <div>Hello, React Project with Vite</div>
     </>
+    </div>
   );
 }
 
