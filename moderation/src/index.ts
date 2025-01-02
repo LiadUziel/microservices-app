a6ab25e3c60ab8d04afa5f9e74922711ddd65da9
Liad Uziel <liadu@rafael.co.il>
2025-01-02 10:02:35 +0200

    Init Express project with error handler

---
+++ moderation/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import { errorHandler } from "./controllers/error.controller";
+
+const app = express();
+const PORT = 3003;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+// Error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
