2caf868ae3ffd6b350c63f8b6b256fbde231cbca
Liad Uziel <liadu@rafael.co.il>
2025-01-02 09:21:43 +0200

    Endpoint - get full posts query

--- query/src/index.ts
+++ query/src/index.ts
@@ -4,13 +4,16 @@ import bodyParser from "body-parser";
 
 import { errorHandler } from "./controllers/error.controller";
 import eventRouter from "./routes/event.route";
+import queryRouter from "./routes/query.route";
 
 const app = express();
 const PORT = 3002;
 
 app.use(bodyParser.json());
 app.use(cors());
+
 app.use("/api/event", eventRouter);
+app.use("/api/query", queryRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
