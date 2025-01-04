2e5e15bab22de2e3bfb109496930ea440691132a
Liad Uziel <liad.uz@gmail.com>
2025-01-04 19:20:47 +0200

    Separate event handling into a helper function

--- query/src/controllers/event.controller.ts
+++ query/src/controllers/event.controller.ts
@@ -2,33 +2,51 @@ import { RequestHandler } from "express";
 import { FULL_POSTS } from "../data/full-posts";
 import { FullPost } from "../models/full-post.model";
 
+export const handleEvent = (
+  type:
+    | "PostCreated"
+    | "CommentCreated"
+    | "CommentModerated"
+    | "CommentUpdated",
+  data: any
+) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    const fullPost: FullPost = { id, title, comments: [] };
+
+    // Add full post to data
+    FULL_POSTS[id] = fullPost;
+  }
+
+  if (type === "CommentCreated") {
+    const { postId, id, content, status } = data;
+
+    const post = FULL_POSTS[postId];
+
+    // Add new comment to relevant post
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { postId, id, content, status } = data;
+
+    const post = FULL_POSTS[postId];
+
+    const comment = post.comments.find((comment) => {
+      comment.id === id;
+    })!;
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
 export const receiveEvent: RequestHandler = async (req, res, next) => {
   try {
     const { type, data } = req.body;
 
-    console.log("Received Event", type);
-    console.log(
-      "🚀 ~ file: event.controller.ts:6 ~ constreceiveEvent:RequestHandler= ~ data:",
-      data
-    );
-
-    if (type === "PostCreated") {
-      const { id, title } = data;
-
-      const fullPost: FullPost = { id, title, comments: [] };
-
-      // Add full post to data
-      FULL_POSTS[id] = fullPost;
-    }
-
-    if (type === "CommentCreated") {
-      const { postId, id, content, status } = data;
-
-      const post = FULL_POSTS[postId];
-
-      // Add new comment to relevant post
-      post.comments.push({ id, content, status });
-    }
+    handleEvent(type, data);
 
     res.send({});
   } catch (err) {
